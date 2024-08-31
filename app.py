b3c97112c57f11675bfb691d70915c2087afb159
jeroenvdmbrugge <jeroenvdmbrugge@gmail.com>
2024-08-31 19:50:07 +0200

    Minor updates to app frontend

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import streamlit as st
 from backend import get_headlines, apply_probabilities, get_final_class, pred
 
-st.title("S&P500 Predictor")
+st.title("S&P 500 Predictor Using NLP")
 st.image("header.png", use_column_width=True)
 st.write("")
 st.markdown("""
-        This web application predicts the S&P 500 index movement for the next trading day. 
-        It analyzes 10 recent headlines using a pretrained transformer model to forecast whether the index will:
-        - Decrease by more than 0.5%\n
-        - Maintain its current level\n
-        - Increase by more than 0.5%\n
-        Click the button below to get started!
-         """)
+        This web application forecasts the movement of the S&P 500 index by the close of the next trading day 
+        based on recent financial news headlines from Yahoo Finance. It uses a fine-tuned transformer model to predict 
+        whether the index will:
+        - increase more than 0.5%
+        - decrease more than 0.5%
+        - maintain its current level
+        
+        For more details, visit the [GitHub repository](https://github.com/jeroenvdmbrugge/SP500-Predictor-WebApp).
+        """)
 
-if st.button('Get Headlines and Predict S&P500 Movement'):
+if st.button("Click Here to Get Started"):
     df = get_headlines()
     st.table(df)
     df = apply_probabilities(df, pred)
     final_class = get_final_class(df)
     if final_class == "decrease":
-        st.info("Our model predicts that the S&P500 will decrease by more than 0.5% on the next trading day.")
+        st.info("The model predicts that the S&P 500 index will decrease more than 0.5% by the close of next trading day.")
     elif final_class == "maintain":
-        st.info("Our model predicts that the S&P500 will maintain its current level on the next trading day.")
+        st.info("The model predicts that the S&P 500 index will maintain its current level by the close of the next trading day.")
     else:
-        st.info("Our model predicts that the S&P500 will increase by more than 0.5% on the next trading day.")
+        st.info("The model predicts that the S&P 500 index will increase more than 0.5% by the close of the next trading day.")
